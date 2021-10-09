23a9330f5ca6deec487b9edcacb582380d6f7ad4
Caio C.R. Garcez <caio.garcez1@gmail.com>
2021-10-09 09:56:08 -0300

    add pickle solution to hold embedding vectors

--- debug/00- gen_simple_embedding.py
+++ debug/00- gen_simple_embedding.py
@@ -1,14 +1,28 @@
+import argparse
+import os
+import pickle
+
 import cv2
 import face_recognition
-import imutils
 
-image = cv2.imread("./images/caio.jpg")
-rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+ap = argparse.ArgumentParser()
+ap.add_argument(
+    "-isrc", "--source", required=False, help="Full path for image source file."
+)
+args = vars(ap.parse_args())
 
-boxes = face_recognition.face_locations(rgb, model="hog")
-face_encoding = face_recognition.face_encodings(image, boxes)[0]
+
+def get_face_encoding(image_path):
+    image = cv2.imread(image_path)
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    boxes = face_recognition.face_locations(rgb, model="hog")
+    return face_recognition.face_encodings(image, boxes)[0]
+
+
+if args["source"]:
+    face_encoding = get_face_encoding(args["source"])
+    with open("output.pkl", "wb") as f:
+        pickle.dump(face_encoding, f)
+
 print(f"[INFO] Embedding vector:\n {face_encoding}\n\n")
 print(f"[INFO] Length: {len(face_encoding)}")
-
-with open("embedding.txt", "w", encoding="utf-8") as f:
-    f.write(str(face_encoding))
